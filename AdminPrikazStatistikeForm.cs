981312882e90cd3b0291c48d93b22645e57db770
Plutcy <rmusvuk@gmail.com>
2024-01-08 18:55:51 +0100

    Solved: issue #6 issue #7 issue #12

---
+++ AdminPrikazStatistikeForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TehnickiPregled
+{
+    public partial class AdminPrikazStatistikeForm : Form
+    {
+        public AdminPrikazStatistikeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AdminPanelForm apf =  new AdminPanelForm();
+            apf.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoginForm lf = new LoginForm();
+            lf.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ MijenjanjeSifreForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TehnickiPregled
+{
+    public partial class MijenjanjeSifreForm : Form
+    {
+        public MijenjanjeSifreForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection("Data Source=VUKOV-KOMPJUTOR\\SQLEXPRESS;Initial Catalog=TehnickiPregledLogin;Integrated Security=True;Encrypt=False"))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("UPDATE login SET password = @newpassword WHERE username = @username AND password = @password", conn);
+
+                cmd.Parameters.AddWithValue("@username", Properties.Settings.Default.logedinuser);
+                cmd.Parameters.AddWithValue("@password", textBox1.Text);
+                cmd.Parameters.AddWithValue("@newpassword", textBox2.Text);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AdminPanelForm apf = new AdminPanelForm();
+            apf.Show();
+            this.Hide();
+        }
+    }
+}
